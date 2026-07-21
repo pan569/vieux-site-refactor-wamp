f2541a335de77b4ef20c44cad2bfcf20c5822590
pan569 <pan569@proton.me>
2026-07-21 12:02:54 +0200

    Phase 6 : form::champCsrf() pour intégrer facilement le token CSRF

--- systeme/vue/form.class.php
+++ systeme/vue/form.class.php
@@ -1,11 +1,15 @@
 <?php
 namespace systeme\vue;
 
+use systeme\securite\Csrf;
+
 /**
  * Class Form
  * Permet de generer un formulaire
- * 
+ *
  * le 08/12/2018 par ulysse1976
+ *
+ * Phase 6 : ajout de champCsrf() pour la protection CSRF.
  */
 class form
 {
@@ -18,17 +22,25 @@ class form
     {
         if(is_null(self::$_instance))
             self::$_instance = new form();
-            
+
             return self::$_instance;
     }
-    
+
     protected $classe;
 
     public function __construct(string $classe = null)
     {
         $this->classe =$classe;
     }
-    
+
+    /**
+     * Champ hidden CSRF à placer dans chaque formulaire POST.
+     * Usage : <?= $form->champCsrf(); ?>
+     */
+    public function champCsrf(string $name = '_csrf'): string
+    {
+        return Csrf::champ($name);
+    }
 
     /**créer un champ pour formulaire
      * doté d'un <label> et d'un <span> pour afficher les erreurs
@@ -41,7 +53,7 @@ class form
      */
     public function ConstructeurChamp(string $titre, string $nom, string $valeur, string $type, array $options=[], string $description=null)
     {
-       
+
         $resultat  = $this->LeLabel($titre, $nom);
         $resultat .= $this->ChoixTypeChamp($nom,$valeur,$type,$options);
         $resultat .= $this->spanErreur($nom);
@@ -55,7 +67,7 @@ class form
         //<?= $messageFlash->getMessage(\systeme\session\MessageFash::_Succes);
         return '<div class=\"'.$css.'\">'.$messageTexte.'</div>';
     }
-    
+
     /**créer un champ pour formulaire de type checkbox ou radio
      * doté d'un <label> et d'un <span> pour afficher les erreurs
      *
@@ -75,7 +87,7 @@ class form
 
         return $resultat;
     }
-    
+
     /**créer un champ pour formulaire de type checkbox ou radio
      * doté d'un <label> et d'un <span> pour afficher les erreurs
      *
@@ -87,12 +99,12 @@ class form
      */
     public function ConstructeurChampSelection_(string $titre, string $nom, string $valeur=null, string $type,$options=[], string $description=null)
     {
-        
+
         $resultat  = $this->ChoixTypeChamp($nom,$valeur,$type,$options);
         $resultat .= $this->LeLabel($titre, $nom);
         $resultat .= $this->spanErreur($nom);
         //$resultat  = $this->BaliseDecoChamp($resultat,"p");
-        
+
         return $resultat;
     }
 
@@ -123,7 +135,7 @@ class form
     {
         switch ($type)
         {
-            case 'text':            
+            case 'text':
             case 'radio':
             case 'color':
             case 'date':
@@ -133,33 +145,33 @@ class form
             case 'url':
                 return "<input type=\"{$type}\" name=\"{$nom}\" id=\"{$nom}\" value=\"{$valeur}\" class=\"{$this->classe}\" />";
                 break;
-                
+
             case 'checkbox':
-                
+
                 $isCheck= false;
-              
+
                 if($valeur != null )
                     $isCheck = 'checked';
-                
-                    
+
+
                 return "<input type=\"{$type}\" name=\"{$nom}\" id=\"{$nom}\" value=\"{$valeur}\" class=\"{$this->classe}\" {$isCheck} />";
                 break;
-                
+
             case 'checkboxList':
-                
+
                 $resultat="";
-                
+
                 //debug($nom,"nom");
                 //debug($valeur,"valeur");
                 //debug($type,"type");
                 //debug($options,"options");
-                
-                $valeurs = explode ( ';' , $valeur  );                
+
+                $valeurs = explode ( ';' , $valeur  );
                 //debug($valeurs,"valeurs");
-                
-                
+
+
                 foreach ($options as $option)
-                {   
+                {
                    //debug($option,"option");
                     if(!empty($option))
                     {
@@ -168,14 +180,14 @@ class form
                         {
                            //debug("{$option} est dans le tableau valeurs");
                            $isCheck = 'checked';
-                           
+
                         }
-                        
+
                         $resultat.= $this->checkboxList($option,"{$nom}_{$option}", $isCheck);
                     }
-                    
+
                 }
-                
+
                 return $resultat;
                 break;
             case 'hidden':
@@ -190,7 +202,7 @@ class form
                 else
                     return "<input type=\"{$type}\" name=\"{$nom}\" id=\"{$nom}\" value=\"{$valeur}\" class=\"{$this->classe}\"/>";
                 break;
-            case 'file':                
+            case 'file':
                 //$accept= implode ( ", " , $options ) ;//"image/png, image/jpeg" accept=\"{$accept}\"
                 return "<input type=\"{$type}\" name=\"{$nom}\" id=\"{$nom}\" class=\"{$this->classe}\"  size=50 />";
                 break;
@@ -201,7 +213,7 @@ class form
                 $width =$options['width'];
                 return "<input type=\"{$type}\" name=\"{$nom}\" value=\"{$nom}\" class=\"{$this->classe}\" alt=\"{$alt}\" src=\"{$src}\" height=\"{$height}\" width=\"{$width}\" />";
                 break;
-            case 'submit':                
+            case 'submit':
                 return "<input type=\"{$type}\" name=\"{$nom}\" value=\"{$valeur}\" class=\"{$this->classe}\" />";
                 break;
             case 'textarea':
@@ -211,73 +223,74 @@ class form
                 break;
             case 'select':
                  $resultat = "<select name=\"{$nom}\" id=\"{$nom}\" class=\"{$this->classe}\" >";
-                                 
+
                  foreach ($options as $c => $v)
                  {
 
                      $sel= null;
-                     
+
                      if($c == $valeur)
                          $sel='selected';
                      $resultat .= "<option value='{$c}' {$sel} >{$v}</option> ";
                  }
-                    
+
                 $resultat .= "</select>";
-                
+
                 return $resultat;
-                
+
                 break;
             case 'selectDur':
                 $resultat = "<select name=\"{$nom}\" id=\"{$nom}\" class=\"{$this->classe}\" >";
-                
+
                 foreach ($options as $option)
                 {
-                    
+
                     $sel= null;
-                    
+
                     if($option == $valeur)
                         $sel='selected';
                         $resultat .= "<option value='{$option}' {$sel} >{$option}</option> ";
                 }
-                
+
                 $resultat .= "</select>";
-                
+
                 return $resultat;
-                
+
                 break;
             case 'selectMultiple':
                 $resultat = "<select name=\"{$nom}\" id=\"{$nom}\" class=\"{$this->classe}\" multiple=\"true\" >";
-                
+
                 //decompose la liste des valeurs a selectioner qui est sous chaine de caractaire en tableau
                 $tabValeur = [];
                 $separateur =';';
                 if(strpos($valeur, $separateur) != false)
                 {
                     $tabValeur = explode ( $separateur , $valeur);
-                    
+
                     //debug($tabValeur,"Conv convTexteVersTable tabValeur");
                 }
                 else
                 {
                     $tabValeur[] = $valeur;
                 }
-                               
+
                 //ajoute toute les option a metre dans la liste selectable et selectionne selon la liste des valeurs
                 foreach ($options as $option)
-                {                    
-                    $sel= null;                    
+                {
+                    $sel= null;
                     if(in_array ( $option , $tabValeur  ))
                     {
-                        $sel='selected';                        
+                        $sel='selected';
+
                     }
-                    
-                    $resultat .= "<option value='{$option}' {$sel} >{$option}</option> ";                    
+
+                    $resultat .= "<option value='{$option}' {$sel} >{$option}</option> ";
                 }
-                
+
                 $resultat .= "</select>";
-                
+
                 return $resultat;
-                
+
                 break;
         }
     }
@@ -312,20 +325,23 @@ class form
     {
         return "<button id=\"{$nom}\"  type=\"{$type}\"  class=\"{$class}\"  value=\"{$valeur}\" >{$titre}</button>";
     }
-    
+
     public function BoutonSupprimer (string $urlAction):string
     {
-        //$urlAction =  $router->generateUri('blogAdmin.suppression', ['id' => $item->get('id')]); 
-        
+        //$urlAction =  $router->generateUri('blogAdmin.suppression', ['id' => $item->get('id')]);
+
+        $csrf = Csrf::champ();
+
         $resultat ="
         <form method=\"post\" action=\"$urlAction\" enctype=\"multipart/form-data\">
         <div class=\"form-group\">
         <input type=\"hidden\" name=\"_method\" value=\"DELETE\">
+        {$csrf}
         <p><button class=\"btn btn-primary\">Supprimer</button></p>
         </div>
         </form>
         ";
-        
+
         return $resultat;
     }
 
@@ -351,33 +367,33 @@ class form
         return "<{$balise}>{$champ}</{$balise}>";
     }
 
-    
+
     public function LienMenu(string $href, string $titre, bool $active = false)
     {
         $lien = $this->Lien($href, $titre , ['nav-link'], null);
-        
+
         if($active)
         {
             $class = 'class="nav-item active"';
         }
-        else 
+        else
         {
             $class = 'class="nav-item"';
         }
-        
-        
+
+
         return "<li $class >{$lien}</li>";
     }
-    
-    public function Lien(string $href, string $titre, string $Css, ?string $src, ?string $cible = null ) 
+
+    public function Lien(string $href, string $titre, string $Css, ?string $src, ?string $cible = null )
     {
-        
-            
+
+
         //$class = implode(" ", $Css);
         $class = $Css;
 
         if($cible != null)
-        { 
+        {
             $target = "target=\"{$cible}\"";
         }
         else
@@ -385,7 +401,7 @@ class form
             $target = null;
         }
 
-        
+
         if($src == null)
         {
             return "<a class=\"{$class}\" href=\"{$href}\" {$target}>{$titre}</a>";
@@ -396,68 +412,68 @@ class form
         }
 
     }
-    
+
     public function link($href)
     {
         return "<link href=\"{$href}\" rel=\"stylesheet\" type=\"text/css\">";
     }
-    
+
     public function script($src)
-    {        
+    {
         //return "<script src=\"{$src}\" async></script>";
         return "<script src=\"{$src}\" ></script>";
     }
-        
+
     public function test01 (/*$titre, $nom, $valeur*/)
     {
-        
+
         $valeur = "Fr;Al;Jn;At;Oe;De";
-        
+
         $tabValeur = explode ( ';' ,  $valeur  );
-        
+
         //debug($tabValeur,"form test01 tabValeur");
-       
-        
-        $list= array('Jr','Fr','Ms','Al','Mi','Jn','Jt','At','Se','Oe','Ne','De');        
+
+
+        $list= array('Jr','Fr','Ms','Al','Mi','Jn','Jt','At','Se','Oe','Ne','De');
         $resultat = NULL;
         foreach ($list as $mois)
         {
-            
+
             if (in_array ( $mois , $tabValeur ,false ))
             {
                 $val = true;
             }
-            else 
+            else
             {
                 $val = false;
             }
-            
+
             //debug($val,$mois);
-            
+
             $resultat .= $this->ConstructeurChampSelection_($mois, $mois, $val , 'checkbox');
         }
-        
+
         $resultat  = $this->BaliseDecoChamp($resultat,"p");
         //
-        
+
         return $resultat;
-        
+
     }
- 
-    
+
+
     public function checkboxList(string $titre, string $nom, string $valeur = null, string $option = null)
     {
-        
+
         //$resultat  = $this->LeLabel( substr($titre, 0, 3) , $nom);
         $resultat  = $this->LeLabel( $titre, $nom);
         $resultat .= $this->ChoixTypeChamp($nom,$valeur,'checkbox',$option);
         //$resultat .= $this->spanErreur($nom);
         //$resultat  = $this->BaliseDecoChamp($resultat,"p");
-        
+
         return $resultat;
     }
 
-    
+
 
 }
 
