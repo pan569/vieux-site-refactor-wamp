76cf12f791be53a6c78898cffe07efaff14365d0
pan569 <pan569@proton.me>
2026-07-22 04:24:38 +0200

    Fix: charger Securite au bootstrap pour que e() soit toujours disponible

--- index.php
+++ index.php
@@ -1,31 +1,30 @@
 <?php
 use systeme\Autoloader;
 use systeme\Noyau;
+use systeme\securite\Securite;
 
 ?>
 
 <?php
 
-
-
 //https://stackoverflow.com/questions/36577020/php-failed-to-open-stream-no-such-file-or-directory
 
-$s=DIRECTORY_SEPARATOR;
+$s = DIRECTORY_SEPARATOR;
 
-require $_SERVER ["DOCUMENT_ROOT"] .$s.'debug.php';
-require $_SERVER ["DOCUMENT_ROOT"] .$s.'systeme'.$s.'Autoloader.class.php';
+require $_SERVER["DOCUMENT_ROOT"] . $s . 'debug.php';
+require $_SERVER["DOCUMENT_ROOT"] . $s . 'systeme' . $s . 'Autoloader.class.php';
 
-
-//## instensiation de l'autoloader ##
+// ## instanciation de l'autoloader ##
 $autoloader = new Autoloader();
 $autoloader->register();
+
+// Charge Securite dès le démarrage → définit la fonction globale e()
+class_exists(Securite::class);
 ?>
 
-<?php 
+<?php
 
 Noyau::getInstance()->navig();
 //Noyau::getInstance()->memAllUrl();
 /**/
 ?>
-
-
